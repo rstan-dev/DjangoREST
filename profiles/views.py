8a98d35dab9bf3389cb2905f44f7ce5dc0ed12db
Russ-S <rstanleym74@gmail.com>
2023-09-23 05:28:43 +0000

    update Profile queryset with .annotate function and add count fields for posts followers and following.  Update Profile Serilaizers

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -10,7 +12,21 @@ class ProfileList(generics.ListAPIView):
     No Create view (post method), as profile creation handled by django signals
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following_created_at',
+        'owner__followed_created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -19,4 +35,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
