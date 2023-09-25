433c04bc01f5bdc964d21dfeefaeae3d61aac603
Russ-S <rstanleym74@gmail.com>
2023-09-25 11:49:38 +0000

    update env.py and settings for cloudinary variable

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -51,6 +51,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
@@ -122,10 +123,6 @@ else:
     ]
 
 CORS_ALLOW_CREDENTIALS = True
-
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
 
 ROOT_URLCONF = 'drf_api.urls'
 
