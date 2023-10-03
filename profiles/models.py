c03954ca10934ca53746afbf74edf2a15890d0f4
Russ-S <rstanleym74@gmail.com>
2023-10-03 13:43:50 +0000

    rename default avatar image

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='https://res.cloudinary.com/dyv8xopsd/image/upload/v1695037025/DjangoREST/default_profile_sqedgc.jpg'
+        upload_to='images/', default='../default_profile'
     )
 
     class Meta:
