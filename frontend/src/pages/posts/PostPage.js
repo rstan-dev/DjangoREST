7dc52e981042abb6d60e4cd5340431b24dc9d75d
Russ-S <rstanleym74@gmail.com>
2023-10-04 17:09:32 +0000

    create comment.js, destructure the props and create the return div to show avatar, link to profile, owners name, updated_at and content

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -8,6 +8,7 @@ import appStyles from "../../App.module.css";
 import {useParams} from "react-router-dom"
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
@@ -57,9 +58,7 @@ function PostPage() {
                 ) : null}
                 {comments.results.length ? (
                   comments.results.map(comment => (
-                    <p key={comment.id}>
-                      {comment.owner}: {comment.content}
-                    </p>
+                    <Comment key={comment.id} {...comment}/>
                   ))
                 ) : currentUser ? (
                   <span>No comments yet, be the first to comment!</span>
