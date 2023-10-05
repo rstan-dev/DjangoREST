5a70a9fb9cfbb8b3eeb7d9656d0848b82ab8bc00
Russ-S <rstanleym74@gmail.com>
2023-10-05 11:53:21 +0000

    add handledelete function to PostPage to delete comment

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -5,6 +5,7 @@ import Avatar from '../../components/Avatar'
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Comment = (props) => {
 
@@ -14,10 +15,32 @@ const Comment = (props) => {
     owner,
     updated_at,
     content,
+    id,
+    setPost,
+    setComments,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`)
+      setPost(prevPost => ({
+        results: [{
+          ...prevPost.results[0],
+          comments_count: prevPost.results[0].comments_count -1
+        }]
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
@@ -33,7 +56,7 @@ const Comment = (props) => {
                 </p>
             </Media.Body>
             {is_owner && (
-              <MoreDropdown handleEdit={() =>{}} handleDelete={() =>{}} />
+              <MoreDropdown handleEdit={() =>{}} handleDelete={handleDelete} />
             )}
 
         </Media>
