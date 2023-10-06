48ba58d711c0a91d4d192196af9acf56962949b0
Russ-S <rstanleym74@gmail.com>
2023-10-06 15:33:53 +0000

    add logic to display follow and unfollow button on ProfilePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostEditForm from "./pages/posts/PostEditForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
+import ProfilePage from "./profiles/ProfilePage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
@@ -52,6 +53,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
